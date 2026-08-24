a4bd6f8194b1267c46fe520dceaa779cb1fe1f1f
Sijan666 <666majharulislam@gmail.com>
2026-08-24 19:15:49 +0600

    refactor : update design

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 
-// reusable button or link component
-const Button = ({ children, className, href, onClick, ...props }) => {
+const Button = ({ children, className, ...props }) => {
     const baseClasses = `bg-[#262626] text-white font-bold cursor-pointer inline-block text-center transition-all ${className}`;
-    
-    // render as anchor link if href exists
-    if (href) {
         return (
-        <a href={href} className={baseClasses} {...props}>
+        <button className={baseClasses} {...props}>
             {children}
-        </a>
+        </button>
         );
     }
 
-    // render as standard button
-    return (
-        <button onClick={onClick} className={baseClasses} {...props}>
-        {children}
-        </button>
-    );
-};
 
 export default Button;
