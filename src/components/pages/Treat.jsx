9bbf212dd64110ec79c708ee1d5eb1f63df7e136
menabil <nabil99cc@gmail.com>
2026-08-25 13:31:07 +0600

    feat: add react helmet in treat page

---
+++ src/components/pages/Treat.jsx
@@ -0,0 +1,20 @@
+import { Helmet } from "react-helmet-async";
+
+const Treat = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Rehab Therapist | What we treat</title>
+        <meta
+          name="description"
+          content="Expert home-based neurological and elderly physiotherapy rehabilitation services across the UK."
+        />
+      </Helmet>
+      <main>
+        
+      </main>
+    </>
+  );
+};
+
+export default Treat;
